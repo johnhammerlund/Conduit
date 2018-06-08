917f9204d7dadcb767f16127739250c08597627a
John Hammerlund <johnhammerlund@gmail.com>
2018-06-08 10:38:40 -0700

    Add test component for coverage

---
+++ Sources/Conduit/Networking/Car.swift
@@ -0,0 +1,31 @@
+//
+//  SampleComponent.swift
+//  Conduit
+//
+//  Created by John Hammerlund on 6/8/18.
+//  Copyright © 2018 MINDBODY. All rights reserved.
+//
+
+import Foundation
+
+final class Car: NSObject {
+
+    var numWheels: Int
+    var make: String
+    var model: String
+
+    init(make: String, model: String, numWheels: Int) {
+        self.make = make
+        self.model = model
+        self.numWheels = numWheels
+    }
+
+    private func pressGasPedal() {
+
+    }
+
+    func drive() {
+        pressGasPedal()
+    }
+
+}
